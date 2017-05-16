9888d9c744506140e821e10d88866016c28d7ab9
EnriqueJRuiz <enriquejruizj@gmail.com>
2017-05-16 18:08:11 +0200

    con mins

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,23 @@
 /**
  * Created by Curso on 16/05/2017.
  */
+
+
+/**
+ * Created by Enrique J. Ruiz on 10/05/2017.
+ */
+const gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+
+gulp.task('compile-js',function() {
+    return browserify('./src/js/scripts.js')
+        //.transform(babelify)
+        .bundle()
+        .pipe(source('all-min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
-var ampliaciones=[1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+var $ = require('jquery');
 $.noConflict();
-jQuery(document).ready(function ($) {
+$(document).ready(function ($) {
     $("#borrartodos").click(function (event) {
         if($(this).is(":checked")){
             $("tbody input[type=checkbox]").prop("checked",true);
@@ -8,37 +8,6 @@ jQuery(document).ready(function ($) {
             $("tbody input[type=checkbox]").prop("checked",false);
         }
     });
-   /* cargarArrayAmpliaciones();
-    function cargarArrayAmpliaciones() {
-
-        if (ampliaciones.length > 0) {
-            for (var a in ampliaciones) {
-                console.log(a);
-                var texto = "<tr>" +
-                        "<td>" +
-                            "<input type='checkbox'  value='" + a + "'>" +
-                        "</td>" +
-                        "<td class=''>" +
-                            a +
-                        "</td>" +
-                        "<td>" +
-
-                        "</td>" +
-                        "<td>" +
-
-                        "</td>" +
-                        "<td>" +
-
-                        "</td>" +
-                    "</tr>";
-                $("#tablaAmpliaciones tbody").append(texto);
-            }
-            $("#tablaAmpliaciones tfoot td").html("<span class='text-error'>Total expansiones:"+ampliaciones.length,10+"</span>");
-        }else{
-            $("#tablaAmpliaciones").remove();
-            $("#listadoAmpliaciones").text("No se han encontrado alumnos")
-        }
-    }*/
     prueba();
     function prueba() {
         const url="http://localhost:8080/bibliotecamtg/api/ampliacipones";
@@ -77,5 +46,6 @@ jQuery(document).ready(function ($) {
                     $("#listadoAmpliaciones").text("No se han encontrado alumnos")
                 }
             });
+
     }
 });
